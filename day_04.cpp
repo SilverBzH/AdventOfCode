477761e8c141606a32145e22f970b3671539a42b
Charlie Boutier <charlie@tempow.com>
2020-12-10 15:53:38 +0100

    4-2 regex mode

--- day_04.cpp
+++ day_04.cpp
@@ -16,9 +16,27 @@ int main() {
             continue;
         }
         smatch match_id;
-        while(regex_search(line, match_id, regex("([a-z]+):#?[0-9a-z]+"))) {
-            if (regex_match(string(match_id[1]), regex("byr|iyr|eyr|hgt|hcl|ecl|pid")))
-                nb_valid_id++;
+        while(regex_search(line, match_id, regex("([a-z]+):(#?[0-9a-z]+)"))) {
+            string id = match_id[1];
+            string data = match_id[2];
+            if ((id == "byr" && stoi(data) >= 1920 && stoi(data) <= 2002) || 
+                (id == "iyr" && stoi(data) >= 2010 && stoi(data) <= 2020) ||
+                (id == "eyr" && stoi(data) >= 2020 && stoi(data) <= 2030) || 
+                (id == "hcl" && regex_match(data, regex("^#[0-9a-f]{6}"))) ||
+                (id == "ecl" && regex_match(data, regex("amb|blu|brn|gry|grn|hzl|oth"))) ||
+                (id == "pid" && regex_match(data, regex("[0-9]{9}")))
+                ) {
+                    nb_valid_id++;
+            } else if (id == "hgt" && regex_match(data, regex("(^[0-9]+)(cm|in)"))) {
+                smatch metrics;
+                regex_search(data, metrics, regex("(^[0-9]+)(cm|in)"));
+                int value = stoi(metrics[1]);
+                string metric = metrics[2];
+                if ((metric == "cm" && value >= 150 && value <= 193) ||
+                    (metric == "in" && value >= 59 && value <= 76)) {
+                        nb_valid_id++;
+                    }
+            }
             line = match_id.suffix();
         }
     }
