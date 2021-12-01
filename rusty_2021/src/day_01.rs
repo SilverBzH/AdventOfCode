0ddc8bcdd1e5d431e890895cb8d162eaa360463a
Charlie Boutier <charliebout@google.com>
2021-12-01 17:09:12 +0000

    day01: Fancy way

--- rusty_2021/src/day_01.rs
+++ rusty_2021/src/day_01.rs
@@ -1,24 +1,12 @@
 pub fn solve(input: String) {
-    let m = input.lines().map(|m| m.parse::<i32>().unwrap());
-    solve1(m.clone());
-    solve2(m);
+    let input = input.lines().map(|m| m.parse::<usize>().unwrap()).collect::<Vec<usize>>();
+    solve0(input.clone(), 2);
+    solve0(input, 4);
 }
 
-fn solve1(mut measurements: impl Iterator<Item = i32>) {
-    let mut prev: i32 = measurements.next().unwrap();
+fn solve0(input: Vec<usize>, size: usize) {
     println!(
-        "day_01: {}",
-        measurements
-            .filter(|m| {
-                let ret = m > &prev;
-                prev = *m;
-                return ret;
-            })
-            .count()
-    );
-}
-
-fn solve2(input: impl Iterator<Item = i32>) {
-    let measurements = input.collect::<Vec<i32>>();
-    solve1(measurements.windows(3).map(|m| m.iter().sum::<i32>()));
+        "Day1: {}",
+        input.windows(size).filter(|m| m.last() > m.first()).count()
+    )
 }
