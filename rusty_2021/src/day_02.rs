59d703dd0e122970d6f109d405783b1879225a61
Charlie Boutier <charliebout@google.com>
2021-12-02 10:10:34 +0000

    day02

---
+++ rusty_2021/src/day_02.rs
@@ -0,0 +1,62 @@
+use crate::lib::Solve;
+
+pub struct Day2 {}
+
+impl Day2 {
+    fn parse(input: String) -> Vec<String> {
+        input
+            .lines()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+    }
+
+    fn solve(input: String, advanced: bool) -> String {
+        let mut depth = 0;
+        let mut horizontal = 0;
+        let mut aim = 0;
+        for data in Day2::parse(input) {
+            let mut split = data.split(' ');
+            let (direction, value) = (split.next().unwrap(), split.next().unwrap());
+            let value = value.parse::<i32>().unwrap();
+            match direction {
+                "forward" => {
+                    horizontal += value;
+                    if advanced {
+                        depth += aim * value;
+                    }
+                }
+                "down" => {
+                    if advanced {
+                        aim += value
+                    } else {
+                        depth += value
+                    }
+                }
+                "up" => {
+                    if advanced {
+                        aim -= value
+                    } else {
+                        depth -= value
+                    }
+                }
+                _ => {}
+            }
+        }
+        let answer = depth * horizontal;
+        answer.to_string()
+    }
+}
+
+impl Solve for Day2 {
+    fn name() -> String {
+        "Dive !".into()
+    }
+
+    fn part_one(input: String) -> String {
+        Day2::solve(input, false)
+    }
+
+    fn part_two(input: String) -> String {
+        Day2::solve(input, true)
+    }
+}
--- rusty_2021/src/main.rs
+++ rusty_2021/src/main.rs
@@ -2,8 +2,10 @@ mod lib;
 use lib::Solve;
 
 mod day_01;
+mod day_02;
 
 fn main() {
     println!("Advent Of Code 11111100101 !");
     day_01::Day1::solve("input/day_01.txt");
+    day_02::Day2::solve("input/day_02.txt");
 }
