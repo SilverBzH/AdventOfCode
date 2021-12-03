f1654fd88cdf893fbc535fa3157357ce4ce15f12
Charlie Boutier <charlie.boutier7@gmail.com>
2021-12-03 11:19:50 +0100

    day3 part one

---
+++ rusty_2021/src/day_03.rs
@@ -0,0 +1,84 @@
+use crate::lib::Solve;
+
+pub struct Day3 {}
+
+impl Day3 {
+    fn solve(input: String) -> String {
+        let len: usize = input.lines().peekable().peek().unwrap().len();
+        let mut counter: Vec<i8> = vec![0; len];
+        let mut gamma: Vec<u8> = Vec::new();
+        for diag in input.lines() {
+            let digit: Vec<char> = diag.chars().collect();
+            for i in 0..len {
+                match digit[i] {
+                    '0' => counter[i] -= 1,
+                    '1' => counter[i] += 1,
+                    _ => unimplemented!(),
+                }
+            }
+        }
+        for c in counter {
+            if c > 0 {
+                gamma.push(1);
+            } else {
+                gamma.push(0);
+            }
+        }
+        let epsilon: Vec<u8> = gamma
+            .iter()
+            .map(|bit| if *bit > 0 { 0 } else { 1 })
+            .collect();
+        let diag = Day3::to_int(&gamma) * Day3::to_int(&epsilon);
+        diag.to_string()
+    }
+
+    fn to_int(data: &[u8]) -> i32 {
+        let mut decimal = 0;
+        let base: i32 = 2;
+        let mut i = 0;
+        data.iter().rev().for_each(|bit| {
+            if *bit > 0 {
+                decimal += base.pow(i);
+            };
+            i += 1;
+        });
+        decimal
+    }
+}
+
+impl Solve for Day3 {
+    fn name() -> String {
+        "Binary Diagnostic".into()
+    }
+
+    fn part_one(input: String) -> String {
+        Day3::solve(input)
+    }
+
+    fn part_two(input: String) -> String {
+        Day3::solve(input)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn day3_1() {
+        let input: String = "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+            .into();
+        assert_eq!("198", Day3::part_one(input));
+    }
+}
--- rusty_2021/src/main.rs
+++ rusty_2021/src/main.rs
@@ -3,9 +3,11 @@ use lib::Solve;
 
 mod day_01;
 mod day_02;
+mod day_03;
 
 fn main() {
     println!("Advent Of Code 11111100101 !");
     day_01::Day1::solve("input/day_01.txt");
     day_02::Day2::solve("input/day_02.txt");
+    day_03::Day3::solve("input/day_03.txt");
 }
